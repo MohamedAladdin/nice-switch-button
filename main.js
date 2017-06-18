c0a05fe37b831374bb6ef4130adeaedfb23d99e4
MohamedAladdin <mohamed_aladdin@live.com>
2017-06-18 02:02:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementsByClassName('the-key')[0].onclick = function () {
+    
+    'use strict';
+    
+    document.getElementsByClassName('the-circle')[0].classList.toggle('off');
+    document.getElementsByClassName('the-circle')[0].classList.toggle('on');
+
+    if (document.getElementsByClassName('the-circle')[0].classList.contains('on')) {
+        
+        this.style.backgroundColor = '#2ecc71';
+        
+    } else {
+        this.style.backgroundColor = '#e74c3c';
+    }
+
+};
